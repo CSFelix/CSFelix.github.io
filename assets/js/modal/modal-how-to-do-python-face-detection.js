b8422d9f5ed38cb6bc84bb2d6f5e56d9b4ca480b
CSFelix <57205604+CSFelix@users.noreply.github.com>
2020-11-19 08:47:07 -0300

    Add files via upload

---
+++ assets/js/modal/modal-how-to-do-python-face-detection.js
@@ -0,0 +1,22 @@
+// Get the modal
+var modal1 = document.getElementById("myModal1");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img1 = document.getElementById("faceDetection");
+
+var modalImg1 = document.getElementById("img01");
+
+//var captionText = document.getElementById("caption");
+
+img1.onclick = function(){
+  modal1.style.display = "block";
+  modalImg1.src = this.src;
+}
+
+///////////////////////
+
+// Get the <span> element that closes the modal
+var span1 = document.getElementById("close1");
+
+// When the user clicks on <span> (x), close the modal
+span1.onclick = function() { modal1.style.display = "none"; }
