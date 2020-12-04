fd98d65c5a9bc9bfebc6e3f84d3fba31c7cc5140
CSFelix <57205604+CSFelix@users.noreply.github.com>
2020-12-04 17:45:45 -0300

    sample-code-function
    sample code function an take off default local storage

--- assets/js/live-editor.js
+++ assets/js/live-editor.js
@@ -16,27 +16,8 @@ function loadCode() {
 			&& cssCode == null
 			&& jsCode == null) {
 
-			htmlCode = "<!DOCTYPE html>"
-							+ "<html>"
-							+ "<head> <meta charset='utf-8'> <meta name='viewport' content='width=device-width,initial-scale=1.0'></head>"
-							+ "<body>"
-							+ "<div class='main'> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> </div>"
-							+ "</body>"
-							+ "</html>"
-			cssCode = "* { margin: 0;	padding: 0; }"
-						+ "body { background-color: ghostwhite; }"
-						+ ".main { width: 100%; height: 100vh; display: flex; justify-content: center; align-items: center; }"
-						+ ".main span { width: 10px; height: 80px; margin: 0 3px; border-radius: 50px; }"
-						+ ".main span:nth-child(5) { background-color: #ff414d; animation: animate0 0.7s infinite; }"
-						+ ".main span:nth-child(4), span:nth-child(6) { background-color: #821752; animation: animate1 0.8s infinite; }"
-						+ ".main span:nth-child(3), span:nth-child(7) { background-color: #f0a500; animation: animate2 0.9s infinite; }"
-						+ ".main span:nth-child(2), span:nth-child(8) { background-color: #07689f; animation: animate3 1s infinite; }"
-						+ ".main span:nth-child(1), span:nth-child(9) { background-color: #00b7c2; animation: animate4 1.1s infinite; }"
-						+ "@keyframes animate0 { 0%, 100% { height: 30px; } 50% { height: 80px; } }"
-						+ "@keyframes animate1 { 0%, 100% { height: 30px; } 50% { height: 100px; } }"
-						+ "@keyframes animate2 { 0%, 100% { height: 30px; } 50% { height: 51px; } }"
-						+ "@keyframes animate3 { 0%, 100% { height: 30px; } 50% { height: 90px; } }"
-						+ "@keyframes animate4 { 0%, 100% { height: 30px; } 50% { height: 80px; } }"
+			htmlCode = ""
+			cssCode = ""
 			jsCode = "";
 		}
 
@@ -52,6 +33,41 @@ function loadCode() {
 }
 
 function openQuestionModal() { $("#questionDescription").toggle(500); }
+
+function sampleCode() {
+
+	// Setting Defult Code
+	htmlCode = "<!DOCTYPE html>"
+		+ "<html>"
+		+ "<head> <meta charset='utf-8'> <meta name='viewport' content='width=device-width,initial-scale=1.0'></head>"
+		+ "<body>"
+		+ "<div class='main'> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> </div>"
+		+ "</body>"
+		+ "</html>"
+
+	cssCode = "* { margin: 0;	padding: 0; }"
+		+ "body { background-color: ghostwhite; }"
+		+ ".main { width: 100%; height: 100vh; display: flex; justify-content: center; align-items: center; }"
+		+ ".main span { width: 10px; height: 80px; margin: 0 3px; border-radius: 50px; }"
+		+ ".main span:nth-child(5) { background-color: #ff414d; animation: animate0 0.7s infinite; }"
+		+ ".main span:nth-child(4), span:nth-child(6) { background-color: #821752; animation: animate1 0.8s infinite; }"
+		+ ".main span:nth-child(3), span:nth-child(7) { background-color: #f0a500; animation: animate2 0.9s infinite; }"
+		+ ".main span:nth-child(2), span:nth-child(8) { background-color: #07689f; animation: animate3 1s infinite; }"
+		+ ".main span:nth-child(1), span:nth-child(9) { background-color: #00b7c2; animation: animate4 1.1s infinite; }"
+		+ "@keyframes animate0 { 0%, 100% { height: 30px; } 50% { height: 80px; } }"
+		+ "@keyframes animate1 { 0%, 100% { height: 30px; } 50% { height: 100px; } }"
+		+ "@keyframes animate2 { 0%, 100% { height: 30px; } 50% { height: 51px; } }"
+		+ "@keyframes animate3 { 0%, 100% { height: 30px; } 50% { height: 90px; } }"
+		+ "@keyframes animate4 { 0%, 100% { height: 30px; } 50% { height: 80px; } }"
+
+	jsCode = "//There is nothing here";
+
+	document.querySelector(".editor #html-code").value = htmlCode;
+	document.querySelector(".editor #css-code").value = cssCode;
+	document.querySelector(".editor #js-code").value = jsCode;
+
+	runCode();
+}
 
 function saveCode() {
 	localStorage.setItem("htmlCode", document.querySelector(".editor #html-code").value);
@@ -129,6 +145,7 @@ function tabCode(code, htmlJsCode) {
 window.addEventListener('load', loadCode);
 
 document.querySelector("#questionButton").addEventListener("click", openQuestionModal);
+document.querySelector("#sampleButton").addEventListener("click", sampleCode);
 document.querySelector("#saveButton").addEventListener("click", saveCode);
 document.querySelector("#deleteButton").addEventListener("click", deleteCode);
 document.querySelector("#downloadButton").addEventListener("click", downloadCode);
