678e5f57617d17d26c46104afea197d6ebc30f5d
CSFelix <57205604+CSFelix@users.noreply.github.com>
2022-10-08 22:47:04 -0300

    📦 Updates

---
+++ assets/js/scripts/link-start.js
@@ -0,0 +1,34 @@
+const linkStartBtn = document.getElementById('linkStartBtn');
+const linkStartVideo = document.getElementById('linkStartVideo');
+const linkStartAudio = new Audio('./assets/videos-audios/link-start.mp3');
+
+function openFullscreen() {
+	/*
+		Opens video on FullScreen automatically
+	*/
+  
+  	/* Chrome, Opera, Vivaldi... */
+	if (linkStartVideo.requestFullscreen) { linkStartVideo.requestFullscreen(); }
+
+  	/* Safari */
+  	else if (linkStartVideo.webkitRequestFullscreen) { linkStartVideo.webkitRequestFullscreen(); }
+  	
+  	/* IE11 */
+  	else if (linkStartVideo.msRequestFullscreen) { linkStartVideo.msRequestFullscreen(); }
+}
+
+linkStartBtn.addEventListener('click', () => {
+	/*
+		When 'old version button' is clicked, the video
+	is open on full screen and played automatically
+	*/
+	alert('We are moving to the Old Version. Enjoy yourself! 😊');
+	linkStartVideo.style.display = 'block';
+	openFullscreen();
+	linkStartAudio.play();	
+});
+
+linkStartVideo.addEventListener('ended', () => {
+	window.location.href = './old/index.html';
+	alert('Loading has been successfully completed! ✔️');
+});
