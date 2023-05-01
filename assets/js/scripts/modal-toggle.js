986e608595974e94796be6f32ba789c8e30de7d3
CSFelix <57205604+CSFelix@users.noreply.github.com>
2023-05-01 13:18:17 -0300

    📦 Updated

--- assets/js/scripts/modal-toggle.js
+++ assets/js/scripts/modal-toggle.js
@@ -24,6 +24,14 @@ const liveProjectsClose = document.getElementById('liveProjectsClose');
 liveProjectsBtn.onclick = function() { liveProjectsModal.style.display = "block"; }
 liveProjectsClose.onclick = function() { liveProjectsModal.style.display = "none"; }
 
+/* Datasets */
+const datasetsModal = document.getElementById('datasetsModal');
+const datasetsBtn = document.getElementById('datasetsBtn');
+const datasetsClose = document.getElementById('datasetsClose');
+
+datasetsBtn.onclick = function() { datasetsModal.style.display = "block"; }
+datasetsClose.onclick = function() { datasetsModal.style.display = "none"; }
+
 /* ML Algorithms */
 const mlAlgorithmsModal = document.getElementById('mlAlgorithmsModal');
 const mlAlgorithmsBtn = document.getElementById('mlAlgorithmsBtn');
@@ -45,6 +53,7 @@ window.onclick = function(event) {
   if (event.target == themesModal) { themesModal.style.display = "none"; }
   else if (event.target == kaggleModal) { kaggleModal.style.display = "none"; }
   else if (event.target == liveProjectsModal) { liveProjectsModal.style.display = "none"; }
+  else if (event.target == datasetsModal) { datasetsModal.style.display = "none"; }
   else if (event.target == mlAlgorithmsModal) { mlAlgorithmsModal.style.display = "none"; }
   else if (event.target == dashboardModal) { dashboardModal.style.display = "none"; }
 }
