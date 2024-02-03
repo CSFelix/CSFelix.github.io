039abe8881a88c256c480d618d20ecff45ae7275
CSFelix <57205604+CSFelix@users.noreply.github.com>
2024-02-03 14:25:09 -0300

    🎉 Added
    - added tooltips logic

---
+++ assets/js/scripts/tooltips.js
@@ -0,0 +1,77 @@
+// 	************
+// 	** Params **
+// 	************
+tippyParams = {
+	content: '',
+	theme: 'light',
+	placement: 'top',
+	arrow: true,
+	animation: 'scale-extreme',
+	inertia: true,
+	duration: 400
+};
+
+
+
+// 	*************************
+// 	** Tooltips - Projects **
+// 	*************************
+tippy('#mlAlgorithmsBtn', {
+	...tippyParams,
+	content: 'Explanation about Machine and Deep Learning Algorithms!',
+});
+
+tippy('#dashboardBtn', {
+	...tippyParams,
+	content: 'Live Dashboards made with Streamlit!',
+});
+
+tippy('#datasetsBtn', {
+	...tippyParams,
+	content: 'Public Dashboards on Kaggle!',
+});
+
+tippy('#kaggleBtn', {
+	...tippyParams,
+	content: 'Data Science Projects with Jupyter Notebook!',
+});
+
+tippy('#liveProjectsBtn', {
+	...tippyParams,
+	content: 'Test out Live Projects (Google Chrome is Recommended)!',
+});
+
+tippy('#dsStudiesBtn', {
+	...tippyParams,
+	content: 'Data Science Contents on GitHub!',
+});
+
+
+
+// 	******************************
+// 	** Tooltips - Social Medias **
+// 	******************************
+tippy('#instagramSocialMediaBtn', {
+	...tippyParams,
+	content: 'Instagram!',
+});
+
+tippy('#githubSocialMediaBtn', {
+	...tippyParams,
+	content: 'GitHub!',
+});
+
+tippy('#kaggleSocialMediaBtn', {
+	...tippyParams,
+	content: 'Kaggle!',
+});
+
+tippy('#linkedinSocialMediaBtn', {
+	...tippyParams,
+	content: 'LinkedIn!',
+});
+
+tippy('#gmailSocialMediaBtn', {
+	...tippyParams,
+	content: 'Gmail!',
+});
